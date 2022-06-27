b741f8b97841a4634b3ac0286d9353b24298e2ac
Marcela Souza <marcelasouza2804@gmail.com>
2022-06-27 18:59:29 -0300

    Estudando tipos

--- src/main/java/br/com/marcelasouza/Main.java
+++ src/main/java/br/com/marcelasouza/Main.java
@@ -2,6 +2,14 @@ package br.com.marcelasouza;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int idade = 1;
+        String nome = "Marcela";
+        double valor = 20.5;
+        boolean verdadeiro = true;
+
+        System.out.println(idade);
+        System.out.println(nome);
+        System.out.println(valor);
+        System.out.println(verdadeiro);
     }
 }
